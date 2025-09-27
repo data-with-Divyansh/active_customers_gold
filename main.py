8aa1a9ae1de02ee9420d4de03d93c3a169ffbb2f
divyanshpatwari <divyanshpatwari36@gmail.com>
2025-09-27 22:14:44 +0530

    Initial commit

--- main.py
+++ main.py
@@ -20,19 +20,24 @@ spark = SparkSession.builder.getOrCreate()
 print("====================== pyspark started ==================")
 print()
 
+# Read customers.csv file and filter active customers only as dataframe
 cust_df = (spark.read.csv("customers.csv",sep=",",header="true")
            .filter("status = 'active'"))
 cust_df.show()
 
+# Read the orders.csv file as dataframe
 order_df = spark.read.csv("orders.csv",sep=",",header="true")
 order_df.show()
 
+# Join both the dataframes
 joindf = cust_df.join(order_df,"customer_id","inner")
 joindf.show()
 
+# Find the total amount each customer has spent
 totaldf = joindf.groupBy("customer_id").agg(sum("amount").alias("total_spend"))
-# totaldf.show()
+totaldf.show()
 
+# Add column to mark customers who spent more than 500 as Gold and remaining as Silver
 join2 = joindf.join(totaldf,"customer_id","left").withColumn("tier",expr("case when total_spend > 500 then 'Gold' else 'Silver' end"))
 join2.show()
 
